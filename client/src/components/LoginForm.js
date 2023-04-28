4a3b7e72fefe2d4c0aee26b015c9f0f14a36c7f9
Eva Reitzel <evareitzel@gmail.com>
2023-04-28 12:04:23 -0400

    Passcode back to regular

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -39,7 +39,7 @@ function LoginForm({ onLogin }) {
         />
       </FormField>
       <FormField>
-        <Label htmlFor="password">Clever Password</Label>
+        <Label htmlFor="password">Password</Label>
         <Input
           type="password"
           id="password"
